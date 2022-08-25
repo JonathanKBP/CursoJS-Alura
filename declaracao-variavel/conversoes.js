b36665af110b50892cfcc54349c49284eeb1d519
Jonathan Kevin <jonathan.pinto@totvs.com.br>
2022-08-25 11:34:27 -0300

    first commit

---
+++ declaracao-variavel/conversoes.js
@@ -0,0 +1,16 @@
+//Tipo de dado
+//booleanos
+
+// conversão implicita
+// const numero = 456;
+// const numeroString = '456';
+
+// console.log(numero + numeroString);
+
+// conversão explícita
+const numero = 456;
+const numeroString = Number('456');
+
+console.log(numero + numeroString);
+
+// console.log(numero + Number(numeroString));
---
+++ tipos-primitivos/type-string.js
@@ -0,0 +1,15 @@
+const texto1 = 'Olá mundo!';
+const texto2 = "Olá mundo!";
+const senha = 'SenhaSuperSegura1234';
+const StringDeNumero = '3566';
+
+const citacao = "Meu nome é ";
+const meuNome = "Jonathan"
+
+// contatenação (+)
+
+console.log(citacao + meuNome);
+
+
+
+//Template string OU template literal
