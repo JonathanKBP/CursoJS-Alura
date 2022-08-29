8f8cce9337257dd4a2d5b4c5a9b3de5ffd4ec604
Jonathan Kevin <jonathan.pinto@totvs.com.br>
2022-08-29 11:28:59 -0300

    Aula template string

---
+++ operadores/template-string.js
@@ -0,0 +1,9 @@
+const nome = "Kevin";
+const idade = 2021 - 1998;
+const cidadeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + ", e nasci na cidade de " + cidadeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao);
