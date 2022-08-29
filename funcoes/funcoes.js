1fc5f399fb7baa126628b7a8881ffcb2be2543d1
Jonathan Kevin <jonathan.pinto@totvs.com.br>
2022-08-29 11:48:57 -0300

    Aula funcoes

---
+++ funcoes/funcoes.js
@@ -0,0 +1,22 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) Declara a função
+                     //string
+function imprimeTexto(texte){
+    console.log(texto);
+}
+
+// 2) Executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("Oi");
+
+//Três formas de escrever funções
+
+function soma(){
+    return 2 + 2;
+}
+
+// console.log(soma());
