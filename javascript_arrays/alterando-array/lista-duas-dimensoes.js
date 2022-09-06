564497def5c01ce5f83ec7012b82360959b6f214
Jonathan Kevin <jonathan.pinto@totvs.com.br>
2022-09-06 11:42:50 -0300

    Aula procurando na lista

--- javascript_arrays/alterando-array/lista-duas-dimensoes.js
+++ javascript_arrays/alterando-array/lista-duas-dimensoes.js
@@ -1,10 +1,10 @@
-const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
 
                       //0
-const mediasDosAlunos = [10,7,9,6]
+const mediasDosAlunos = [10,7,9,6];
 
 // let listaDeAlunos = [['João', 'Juliana', 'Caio', 'Ana'], [10,7,9,6]]
                           //0       //1
-let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
+let listaDeNotasEAlunos = [alunos, mediasDosAlunos];
 
-console.log(`${listaDeNotasEAlunos[0][0]}, sua media é ${listaDeNotasEAlunos[1][0]}`)
+console.log(`${listaDeNotasEAlunos[0][0]}, sua media é ${listaDeNotasEAlunos[1][0]}`);
